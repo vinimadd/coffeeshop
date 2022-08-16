d7640b5f03d16128e8cec4da4e2aea7fe92c1bc3
vinimadd <vinimadd@gmail.com>
2022-08-16 14:59:34 +0200

    Add basic view.
    Web page is a visual representation of a project.
    
    Resolve #3

--- coffeeshop/src/test/java/com/example/coffeeshop/CoffeeshopApplicationTests.java
+++ coffeeshop/src/test/java/com/example/coffeeshop/CoffeeshopApplicationTests.java
@@ -1,13 +1,37 @@
 package com.example.coffeeshop;
 
+import com.example.coffeeshop.controller.HomeController;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
 class CoffeeshopApplicationTests {
+
+	@Autowired
+	MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testHomePage() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("home"))
+				.andExpect(content().string(containsString("Welcome to our coffee store!")));
 	}
 
 }
