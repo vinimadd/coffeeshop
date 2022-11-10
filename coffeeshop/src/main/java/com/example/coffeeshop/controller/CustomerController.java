b2006c1c46ace61771673bb22a2ef5e899439eda
vinimadd <vinimadd@gmail.com>
2022-11-10 10:02:53 +0100

    update controller for customer Resolve #16

--- coffeeshop/src/main/java/com/example/coffeeshop/controller/CustomerController.java
+++ coffeeshop/src/main/java/com/example/coffeeshop/controller/CustomerController.java
@@ -8,42 +8,59 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.security.auth.login.AccountNotFoundException;
+import javax.validation.Valid;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 @Slf4j
 @Controller
-@RequestMapping("/api/customers")
+@RequestMapping("/api")
 public class CustomerController {
 
     //show customer info
     //show customer orders
 
-    @Autowired
     private CustomerService service;
 
-    @GetMapping
+    public CustomerController(CustomerService service) {
+        this.service = service;
+    }
+
+
+    @GetMapping("/form")
+    public String showForm(Model model){
+        model.addAttribute("customer", new Customer());
+        return "form";
+    }
+
+    @PostMapping("/form")
+    public String addCustomer(@Valid Customer customer, Errors errors) {
+        Customer saveCustomer = null;
+        if (!errors.hasErrors()) {
+            saveCustomer = service.saveCustomer(customer);
+            return "redirect:home";
+        }
+        return "form";
+    }
+
+    @GetMapping("/customers")
     public ResponseEntity<List<Customer>> showAllCustomers(){
 
         List<Customer> myCustomers = service.showAllCustomers();
 
-        if(myCustomers.isEmpty()) {
+        if (myCustomers.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
         return new ResponseEntity<>(myCustomers, HttpStatus.OK);
     }
 
-    @PostMapping
-    public ResponseEntity<Customer> addCustomer(@RequestBody Customer customer) {
-        Customer saveCustomer = service.saveCustomer(customer);
-
-        return new ResponseEntity<>(saveCustomer, HttpStatus.OK);
-    }
-
-    @GetMapping("/{id}")
+    @GetMapping("customers/{id}")
     public ResponseEntity<Customer> getCustomer(@PathVariable("id") int id) {
 
         try {
@@ -54,7 +71,7 @@ public class CustomerController {
         }
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/customers/{id}")
     public ResponseEntity<Customer> updateCustomer(@PathVariable("id") int id, @RequestBody Customer customer) {
 
         try {
@@ -67,7 +84,7 @@ public class CustomerController {
         }
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/customers/{id}")
     public ResponseEntity<Customer> deleteCustomer(@PathVariable("id") int id){
 
         try {
--- coffeeshop/src/main/java/com/example/coffeeshop/entity/Customer.java
+++ coffeeshop/src/main/java/com/example/coffeeshop/entity/Customer.java
@@ -1,11 +1,17 @@
 package com.example.coffeeshop.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "customer")
 public class Customer {
@@ -13,23 +19,29 @@ public class Customer {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     @Column(name="id")
-    private int id;
+    private Integer id;
 
+    @NotEmpty(message = "First name field can not be empty")
     @Column(name = "first_name")
     private String firstName;
 
+    @NotEmpty(message = "Last name field can not be empty")
     @Column(name = "last_name")
     private String lastName;
 
+    @NotEmpty(message = "Address field can not be empty")
     @Column(name = "address")
     private String address;
 
+    @NotEmpty(message = "City field can not be empty")
     @Column(name = "city")
     private String city;
 
+    @Email(message = "Email must be valid")
     @Column(name = "email")
     private String emailAddress;
 
+    @NotEmpty(message = "Phone field can not be empty")
     @Column(name = "phone")
     private String phoneNumber;
 
